93fd0b89acd53a73f5e85445f1281cb3b1b2ae1d
Elison de Campos <elison.campos@gmail.com>
2023-10-04 13:38:37 -0300

    :bug: fix project initialization

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -1,3 +1,3 @@
 set -eux
 
-NEW_RELIC_CONFIG_FILE=app/newrelic.ini newrelic-admin run-program gunicorn --workers=1 --timeout=5400 --bind=0.0.0.0:5000 --access-logfile=- --error-logfile=- app:create_app\(\)
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn --workers=1 --timeout=5400 --bind=0.0.0.0:5000 --access-logfile=- --error-logfile=- app:create_app\(\)
